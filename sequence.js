2ed305721b774b7ec8b7c7ae486451919dd46b1b
Jason Goldberger <jlgoldb2@asu.edu>
2014-03-25 17:16:40 -0700

    Add seqLength paramater to genRandomSeq

--- sequence.js
+++ sequence.js
@@ -1,11 +1,15 @@
 
+// generate a pseudo random sequence
 var genRandomSeq = function(seqLength) {
 	var dnaBases = ["A", "T", "G", "C"];
 	var ampSeqPlus = "";
-	for (i = 0; i < 64; i++) {
+	for (i = 0; i < seqLength; i++) {
 		ampSeqPlus = ampSeqPlus + dnaBases[Math.floor(Math.random() * dnaBases.length) ];
 	}
 	return ampSeqPlus;
 };
+
+
+
 
 
