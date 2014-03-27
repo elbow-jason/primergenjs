6f2d75b44ac4d27e5f6231dcd16d6cb263851919
Jason Goldberger <jlgoldb2@asu.edu>
2014-03-26 17:55:22 -0700

    added genPrimer func and added todo.txt

--- sequence.js
+++ sequence.js
@@ -1,6 +1,38 @@
 
+var sequence = {};
+
+
+
+
+
+//it is necessary to determine which pole of the DNA...
+//sequence is to the left and which is to the right...
+//the default is 5'-DNAHERE-3'.
+//if not oriented properly..
+// primers won't function for PCR
+/*" By convention, if the base sequence of a single strand of DNA is given,
+ the left end of the sequence is 5' end, while the right end of the
+  sequence is the 3' end."
+AND
+"All known DNA replication systems require a free 3' hydroxyl group
+ before synthesis can be initiated"
+*/
+
+//returns substring from beginning of seq (in 5'-to-3' format)
+//Forward primer is oriented the same as original sequence.
+sequence.genPrimer = function(primerStart, primerLength, forward) {
+	if (forward){
+		return sequence.actual.substring(primerStart, primerStart+primerLength);
+	}
+	else {
+		return sequence.genComplement(sequence.actual.substring(sequence.actual.length-primerStart, sequence.actual.length - (primerStart+primerLength)));	
+	}
+//gets substring from end of seq then returns complement in (3'-to-5' format)
+//Reverse primer must be oriented 5'-to-3' prior to chemical evaluation
+};
+
 //function to generate a pseudo random DNA sequence (non-degenerate bases)
-var genRandomSeq = function(seqLength) {
+sequence.genRandomSeq = function(seqLength) {
 	var dnaBases = ["A", "T", "G", "C"];
 	var ampSeqPlus = "";
 	for (i = 0; i < seqLength; i++) {
@@ -13,7 +45,7 @@ var genRandomSeq = function(seqLength) {
 //	sequence = "ATTTTAGCGATCCC"
 //	returns  = "TAAAATCGCTAGGG"
 // f_seq is for "forward sequence"
-var genComplement = function(f_seq) {
+sequence.genComplement = function(f_seq) {
 	var compliment = "";
 	var decoder = { 
 		"T": "A",
@@ -30,7 +62,7 @@ var genComplement = function(f_seq) {
 //function that validates sequence integrity
 	//only A,G,T, and C bases => true
 	//bases other than A,G,T, or C => false
-var seqIntegrityCheck = function (sequence) {
+sequence.seqIntegrityCheck = function (sequence) {
 	var checker = 0;
 	checker = sequence.search(/[~ATGC]/);
 	if (checker === 0) {return true;}
@@ -39,3 +71,8 @@ var seqIntegrityCheck = function (sequence) {
 
 
 
+seqLength = 64;
+// later, this will be user input via form field or accession number.
+sequence.actual = sequence.genRandomSeq(seqLength);
+sequence.forwardPrimer = sequence.genPrimer(0, 18, true);
+sequence.reversePrimer = sequence.genPrimer(0, 21, false);
--- test/test.js
+++ test/test.js
@@ -1,1 +1,14 @@
 // It's empty because I am figuring it out. Hold the Horses.
+
+seqtest = require('../sequence')
+
+describe 'genRandomSeq()', ->
+
+
+
+
+
+
+describe 'genComplement()',
+
+describe 'seqIntegrityCheck()',
